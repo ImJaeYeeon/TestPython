aaa687f7b1a625a8f1cd8b8c01e095f8ab4ecd35
ImJaeYeeon <w2109@e-mirim.hs.kr>
2022-04-13 18:32:56 +0900

    python day1

--- test1.py
+++ test1.py
@@ -1,1 +1,28 @@
 print("안녕")
+
+num = 1 # 정수
+print(num)
+
+num2 = 1.2 # 실수
+print(num2)
+
+str = "임재연" # 문자열
+print(str)
+
+arr = [1,2,3,3,3,3] # 배열
+print(arr)
+
+dic = {1,2,3,3,3,3,3,3} # 딕셔너리
+print(dic)
+
+num3=10
+num4=20
+if 10>20:
+    print("여자")
+else :
+    print("남자")
+
+if num3>num4:
+    print("10")
+else :
+    print("20")
